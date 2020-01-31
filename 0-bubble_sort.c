d7894fb69c8d2d0fb3c0e3b4e7693c702c6ac0ad
Feli <srgatoiscool@gmail.com>
2020-01-31 13:51:47 -0500

    adde first and readme

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Write a function that sorts an array of integers
+ * in ascending order using the Bubble sort algorithm
+ * @array: array with the numbers
+ * @size: len of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i = 0;
+	_Bool swapped = false;
+
+	do {
+		swapped = false;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap(&array[i], &array[i + 1]);
+				print_array(array, size);
+				swapped = true;
+			}
+		}
+	} while (swapped);
+}
+
+/**
+ * swap - Swap func to swap
+ * @a: storage first val
+ * @b: storage second val
+ */
+void swap(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
---
+++ 2-selection_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Write a function that sorts an array of integers
+ * in ascending order using the Selection sort algorithm
+ * @array: array with the numbers
+ * @size: len of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+
+}
